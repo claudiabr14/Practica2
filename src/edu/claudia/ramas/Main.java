f9fe6d1be2f4a8c39de0c11369b0228603e40518
cbenriv1305 <cbenriv1305@g.educaand.es>
2025-11-19 09:10:44 +0100

    Mensaje en consola (rama feature/mensaje)

--- src/edu/claudia/ramas/Main.java
+++ src/edu/claudia/ramas/Main.java
@@ -16,6 +16,6 @@ public class Main {
         System.out.println("Complementario: " + complementario);
         int suma = bolas.stream().mapToInt(Integer::intValue).sum();
         System.out.println("Suma de principales: " + suma);
-        System.out.println("¡Suerte en el sorteo, a ganar!!, DAM!");
+        System.out.println("¡Suerte en el sorteo, DAM!");
     }
 }
